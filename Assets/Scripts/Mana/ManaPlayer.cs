749d604646cbd95a3d89a3eb1773a7ff2a19121f
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-10-08 22:10:50 +0300

    Mana and ManaText
    функцию по уменьшению маны надо будет поставить в место появления фаербола и префаб фаербола надо засунуть в переменную в скрипте HPPlayer на персонаже

---
+++ Assets/Scripts/Mana/ManaPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaPlayer : MonoBehaviour {
+    public static float manaPlayer = 100f;
+    private float time = 0f;
+    void Update() {
+        if (manaPlayer < 100) {
+            RecoveryMana();
+        }
+    }
+    public void DecrementMana() {
+        manaPlayer -= 20f;
+        ManaText.ChangeManaText(manaPlayer);
+    } //вызов этой функции в месте где instantiate у фаербола
+    public void RecoveryMana() {
+        time += Time.deltaTime;
+        if (time > 1) {
+            manaPlayer += 1f;
+            time = 0f;
+            ManaText.ChangeManaText(manaPlayer);
+        }
+    }
+}
---
+++ Assets/Scripts/Mana/ManaText.cs
@@ -0,0 +1,10 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ManaText : MonoBehaviour
+{
+    public static void ChangeManaText(float manaPlayer) {
+        GameObject.Find("ManaText").GetComponent<Text>().text = $"{manaPlayer}";
+    }
+    
+}
