6bf61885da37c2b64952115ad35ff3077084f8e3
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-10-17 22:30:57 +0300

    Bag with buttons fixed

--- Assets/Scripts/Movement/MouseLook.cs
+++ Assets/Scripts/Movement/MouseLook.cs
@@ -12,6 +12,7 @@ public class MouseLook : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        buttonsHide = GetComponent<ButtonsHide>();
     }
     void Update()
     {
