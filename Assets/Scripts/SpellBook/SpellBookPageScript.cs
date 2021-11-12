494f0454647798470e2a2746c8a64e99bffd5b9d
GmaB-a <vladik11012006@gmail.com>
2021-11-12 19:22:06 +0200

    spell book 1

---
+++ Assets/Scripts/SpellBook/SpellBookPageScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellBookPageScript : MonoBehaviour
+{
+    [SerializeField] private SpellBookPageDataScript ThisSpellData;
+    private Text SpellName;
+    private Image SpellIcon;
+    private Text SpellDescription;
+    private Text SpellManaCost;
+    private Text SpellDamage;
+    private Image SpellCastingCode;
+    private void Start()
+    {
+        SpellName = gameObject.transform.Find("SpellName").GetComponent<Text>();
+        SpellIcon = gameObject.transform.Find("SpellIcon").GetComponent<Image>();
+        SpellDescription = gameObject.transform.Find("SpellDescription").GetComponent<Text>();
+        SpellManaCost = gameObject.transform.Find("SpellManaCost").GetComponent<Text>();
+        SpellDamage = gameObject.transform.Find("SpellDamage").GetComponent<Text>();
+        SpellCastingCode = gameObject.transform.Find("SpellCastingCode").GetComponent<Image>();
+
+        SpellName.text = ThisSpellData.SpellName;
+        SpellIcon.sprite = ThisSpellData.SpellImage;
+        SpellDescription.text = ThisSpellData.SpellDescription;
+        SpellManaCost.text = ThisSpellData.SpellManaCost;
+        SpellDamage.text = ThisSpellData.SpellDamage;
+        SpellCastingCode.sprite = ThisSpellData.SpellCastingCode;
+    }
+}
