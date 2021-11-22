f423c6f100a3b006e20a69962ef3f675ab99a994
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-11-22 19:49:23 +0200

    teleport player fixed

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class RoundManager : MonoBehaviour {
+public class LifeManager : MonoBehaviour {
     private GameObject Player;
     private PlayerHP playerHP;
 
@@ -17,14 +17,15 @@ public class RoundManager : MonoBehaviour {
         playerHP = Player.GetComponent<PlayerHP>();
     }
 
-    public void EndRound() {
+    public void EndLife() {
         playerHP.CurrentHealth = playerHP.MaxHealth;
-        if(PhotonNetwork.IsMasterClient)
-        {
-            photonView.RPC("TeleportToSpawnPoint", RpcTarget.All);
-        }
+        TeleportToSpawnPoint();
+        //if(PhotonNetwork.IsMasterClient)
+        //{
+        //    photonView.RPC("TeleportToSpawnPoint", RpcTarget.All);
+        //}
     }
-    [PunRPC]
+    //[PunRPC]
     private void TeleportToSpawnPoint()
     {
         Player.transform.position = spawnPoint.transform.position;
