4a996cc8e1e4a164c84cee5bf115c050159047ee
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2021-12-20 13:52:36 +0300

    Roomlist bug fixed

--- Assets/Scripts/UI/Menu/RoomList/RoomListing.cs
+++ Assets/Scripts/UI/Menu/RoomList/RoomListing.cs
@@ -7,18 +7,24 @@ public class RoomListing : MonoBehaviour
 {
     [SerializeField] private Text text;
 
-    private string RoomName;
+    public RoomInfo roomInfo;
 
     public void SetRoomInfo(RoomInfo info)
     {
         int MaxPlayers = info.MaxPlayers - 1;
         text.text += info.Name + " " + info.PlayerCount + "/" + MaxPlayers;
-        RoomName = info.Name;
+        roomInfo = info;
     }
 
     public void JoinRoom()
     {
-        if(RoomName!=null)
-        PhotonNetwork.JoinRoom(RoomName);
+        if(roomInfo!=null)
+        PhotonNetwork.JoinRoom(roomInfo.Name);
+    }
+
+    private void Update()
+    {
+        if (roomInfo.MaxPlayers <= 0)
+            Destroy(gameObject);
     }
 }
