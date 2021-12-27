b567f3b7367bc5f9993b7c3de62de8b2ebcca197
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2021-12-27 22:37:53 +0300

    CommandPlanes sound

--- Assets/Scripts/UI/Game/TeamPanels.cs
+++ Assets/Scripts/UI/Game/TeamPanels.cs
@@ -5,10 +5,17 @@ public class TeamPanels : MonoBehaviour
     public GameObject yourTeamPanel;
     public GameObject enemyTeamPanel;
     private KeyCode codeActivityPanel = KeyCode.Tab;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(codeActivityPanel)) {
             if (yourTeamPanel.activeSelf == true) {
+                audioSource.Play();
                 yourTeamPanel.SetActive(false);
                 enemyTeamPanel.SetActive(false);
             } else {
