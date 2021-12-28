7aa814a257922fbcd8694f40769727687cefdcf4
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2021-12-28 22:19:42 +0200

    low resolution baking

--- Assets/Scripts/AudioManagers/PlayerAudioManager.cs
+++ Assets/Scripts/AudioManagers/PlayerAudioManager.cs
@@ -1,46 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class PlayerAudioManager : MonoBehaviour
-{
+public class PlayerAudioManager : MonoBehaviour {
     [SerializeField] AudioClip[] FootstepSounds;
 
     private AudioSource audioSource;
     private float StepChangeTime = 2;
     private float StepChangeCooldown = 0;
     private PlayerMovement playerMovement;
+    private PhotonView photonView;
 
-    private void Start()
-    {
+    private void Start() {
         audioSource = GetComponent<AudioSource>();
         playerMovement = GetComponentInParent<PlayerMovement>();
+        photonView = GetComponent<PhotonView>();
+        audioSource.mute = true;
     }
 
-    private void Update()
-    {
-        PlayFootsteps();
-        SetRandomFootstepSFX();
-    }
-
-    private void PlayFootsteps()
-    {
-        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && playerMovement.isGrounded)
-        {
-            audioSource.mute = false;
+    private void Update() {
+        //PlayFootsteps();
+        //SetRandomFootstepSFX();
+        if (photonView != null && playerMovement.isActiveAndEnabled) {
+            if (photonView.IsMine) {
+                photonView.RPC("PlayFootsteps", RpcTarget.All);
+                SetRandomFootstepSFX();
+            }
         }
-        else 
-        {
+    }
+    [PunRPC]
+    public void PlayFootsteps() {
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && playerMovement.isGrounded) {
+            audioSource.mute = false;
+        } else {
             audioSource.mute = true;
         }
     }
 
-    private void SetRandomFootstepSFX()
-    {
+    private void SetRandomFootstepSFX() {
         StepChangeCooldown += Time.deltaTime;
 
-        if(StepChangeCooldown>=StepChangeTime)
-        {
+        if (StepChangeCooldown >= StepChangeTime) {
             audioSource.clip = FootstepSounds[Random.Range(0, FootstepSounds.Length - 1)];
             audioSource.Play();
             StepChangeCooldown = 0;
