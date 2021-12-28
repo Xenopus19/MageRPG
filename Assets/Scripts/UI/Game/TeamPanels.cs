25978d7370dd918b574fd56e372607a4159ac1d0
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2021-12-28 11:24:38 +0300

    Reverted commit

--- Assets/Scripts/UI/Game/TeamPanels.cs
+++ Assets/Scripts/UI/Game/TeamPanels.cs
@@ -5,17 +5,10 @@ public class TeamPanels : MonoBehaviour
     public GameObject yourTeamPanel;
     public GameObject enemyTeamPanel;
     private KeyCode codeActivityPanel = KeyCode.Tab;
-    private AudioSource audioSource;
-
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
     void Update()
     {
         if (Input.GetKeyDown(codeActivityPanel)) {
             if (yourTeamPanel.activeSelf == true) {
-                audioSource.Play();
                 yourTeamPanel.SetActive(false);
                 enemyTeamPanel.SetActive(false);
             } else {
