a4de67b63830ab91df0ca8ce49d5a1f37257f84f
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2022-01-10 18:57:16 +0300

    Resolution settings WIP

--- Assets/Scripts/UI/Settings/GraphicsSettingsScript.cs
+++ Assets/Scripts/UI/Settings/GraphicsSettingsScript.cs
@@ -1,9 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using System.Linq;
 
 public class GraphicsSettingsScript : MonoBehaviour
 {
+    [Header("Settings Objects")]
+    [SerializeField] private Toggle FullscreenToggleObject;
+    [SerializeField] private Dropdown QualitySettingsObject;
+    [SerializeField] private Dropdown ResolutionDropdown;
+
+    private void Start()
+    {
+        FullscreenToggleObject.isOn = Screen.fullScreen;
+        QualitySettingsObject.value = QualitySettings.GetQualityLevel();
+        AddResolutionOptions();
+        
+    }
+    private void AddResolutionOptions()
+    {
+        Resolution[] res = Screen.resolutions;
+        Resolution[] resolutions = res.Distinct().ToArray();
+
+        string[] stringResolutions = new string[resolutions.Length];
+
+        for (int i = 0; i < stringResolutions.Length; i++)
+        {
+            stringResolutions[i] = resolutions[i].ToString();
+        }
+
+        ResolutionDropdown.ClearOptions();
+        ResolutionDropdown.AddOptions(stringResolutions.ToList());
+    }
+    public void SetResolution()
+    {
+        //Screen.SetResolution();
+    }
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
